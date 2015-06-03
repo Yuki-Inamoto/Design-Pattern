41bd5b1a45bef5a467ca0ccdf5495eefb6ddf0eb
Yuki-Inamoto <inamoto.yuki@tis.co.jp>
2015-06-03 17:36:55 +0900

    Result修正

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     factory = IDCardFactory()
 
     card1 = factory.create("Yamada Taro")
-    card2 = factory.create("Yamada Jiro")
-    card3 = factory.create("Yamada Saburo")
+    card2 = factory.create("Kato Jiro")
+    card3 = factory.create("Hudo Saburo")
 
     card1.use()
     card2.use()
